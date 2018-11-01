f53998b8a6a622f7ae02b9896dbad4fd08c2da6f
CitrixQian <sijieqian@foxmail.com>
2018-11-01 21:40:12 +0800

    增加utils类以及插入排序算法

---
+++ src/week_2/Elementary_Sorts/Example.java
@@ -0,0 +1,46 @@
+package week_2.Elementary_Sorts;
+
+import util.StdIn;
+import util.StdOut;
+
+/**
+ * Created by Citrix on 2018/11/1.
+ */
+public class Example {
+    public static void sort(Comparable[] a) {
+
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + "");
+        }
+        StdOut.println();
+    }
+
+    private static boolean isSorted(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] a = StdIn.readAllStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
